abc54e4c61c0ddcac3aaa7e852b5f8b6d00cf115
RezieqFadillah <m.rezieq.f@gmail.com>
2022-12-01 11:53:16 +0800

    tambah user di forum

--- models/forum.js
+++ models/forum.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose
 
 const userSchema = new Schema({
+  user: {
+    type: mongoose.ObjectId,
+    ref: "User"
+  },
   judul: {
     type: String,
     required: true
