55ead7d5ea461024fe2caa708d67747142bbbf75
RezieqFadillah <m.rezieq.f@gmail.com>
2022-12-01 16:53:42 +0800

    Fix balas komentar

--- controllers/komentar.controller.js
+++ controllers/komentar.controller.js
@@ -7,16 +7,23 @@ module.exports = {
     addkomentar: async (req, res) => {
         try {
             const data = req.body
+            const asal = data.id_komentar 
             const Komen = new komen(data)
-            Komen.save()
-    
             const id = req.params.id
             const Forum = await forum.findOne({_id:id})
-            Forum.komentar.push(Komen)
-            await Forum.save()
-    
+
+            if(!asal){    
+                Komen.save()   
+                Forum.komentar.push(Komen)
+               await Forum.save()
+            }else{
+
+                const Komen_Asal = await komen.findOne({_id:asal})
+                Komen_Asal.balasan.push(Komen)
+                await Komen_Asal.save()
+            }
             res.json({
-                data:Forum
+                data:data
             })
         } catch (error) {
             return res.status(400).send("Aduh eror")
